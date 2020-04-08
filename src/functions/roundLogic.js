2570f7708c2275f35abb7aac48bafb1621a9aae8
martin <tin.maldonado@gmail.com>
2020-04-08 17:36:28 -0300

    added await to some functions

--- src/functions/roundLogic.js
+++ src/functions/roundLogic.js
@@ -13,29 +13,29 @@ const roundLogic = async (playerId, partyId, lynchedPlayerId) => {
             if (!player.wasShot) {
                 if (player.wasArmCutOff) {
                     // Player was not healed and arm was cut off
-                    globalAnnouncement(partyId, `Le han cortado el brazo a ${player.playerName}.`)
+                    await globalAnnouncement(partyId, `Le han cortado el brazo a ${player.playerName}.`)
                 }
                 if (player.wasTongueCutOff) {
                     // Player was not healed and tongue was cut off
-                    globalAnnouncement(partyId, `Le han cortado la lengua a ${player.playerName}.`)
+                    await globalAnnouncement(partyId, `Le han cortado la lengua a ${player.playerName}.`)
                 }
-                return reinitializeNightRole(playerId, player.role, player.wasHealed)
+                return await reinitializeNightRole(playerId, player.role, player.wasHealed)
             } else if (player.wasShot) {
                 // Player was not healed and was shot = dies
-                killPlayer(playerId, partyId, player.sided)
-                return globalAnnouncement(partyId, `${player.playerName} ha muerto de un disparo!`)
+                await killPlayer(playerId, partyId, player.sided)
+                return await globalAnnouncement(partyId, `${player.playerName} ha muerto de un disparo!`)
             }
         } else if (player.wasHealed) {
             // Player was healed = cures everything
-            return reinitializeNightRole(playerId, player.role, player.wasHealed)          
+            return await reinitializeNightRole(playerId, player.role, player.wasHealed)          
         }
     } else if (party.instance === 'day') {
         if (lynchedPlayerId === playerId) {
-            killPlayer(playerId, partyId, player.sided)
-            return globalAnnouncement(partyId, `${player.playerName} ha sido linchado por el pueblo!`)
+            await killPlayer(playerId, partyId, player.sided)
+            return await globalAnnouncement(partyId, `${player.playerName} ha sido linchado por el pueblo!`)
         }
         // There was no lynching or I was not the lynched player
-        return reinitializeDayRole(playerId)
+        return await reinitializeDayRole(playerId)
     }
 }
 
